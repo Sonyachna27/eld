c0dbafb2126078c0dbd8c85bcf820f99152cff4d
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-10-19 22:15:41 +0300

    add sections

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+document.addEventListener("DOMContentLoaded", () =>{
+	openHeader();
+	windowLoad();
+});
+
+const openHeader = () =>{
+	const htmlElement = document.querySelector('html');
+	const burgerBtn = document.querySelector('.burger');
+const navLinks = document.querySelectorAll('nav ul a');
+burgerBtn.addEventListener('click', () =>{
+	htmlElement.classList.toggle('open');
+});
+
+navLinks.forEach((link) =>{
+	link.addEventListener('click', () =>{
+		htmlElement.classList.remove('open');
+	})
+})
+}
+function windowLoad() {
+
+	function statValueInit(statValues) {
+		let values = statValues ? statValues : document.querySelectorAll('.stat-value');
+		if (values) {
+			values.forEach(statValue => {
+				numScroll(statValue);
+			})
+		}
+	}
+
+	let observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const targetElement = entry.target;
+				const statValues = targetElement.querySelectorAll('.stat-value');
+				if (statValues.length) {
+					statValueInit(statValues);
+				}
+			}
+		})
+	}, { threshold: 0.3 });
+
+	let sections = document.querySelectorAll('.about__container-bottom-item');
+	if (sections.length) {
+		sections.forEach(section => {
+			observer.observe(section);
+		})
+	}
+
+	function numScroll(statValue) {
+		let zeroValues = () => {
+			statValue.innerHTML = 0;
+		}
+
+		const animationDuration = 3000;
+		const frameDuration = 1000 / 60;
+		const totalFrames = Math.round(animationDuration / frameDuration);
+		const easeOutQuad = t => t * (2 - t);
+
+		const animateCountUp = () => {
+			let frame = 0;
+			const countTo = parseInt(statValue.dataset.target.replace(/,/g, ''), 10);
+
+			const counter = setInterval(() => {
+				frame++;
+				const progress = easeOutQuad(frame / totalFrames);
+				const currentCount = Math.round(countTo * progress);
+
+				if (parseInt(statValue.innerHTML, 10) !== currentCount) {
+					statValue.innerHTML = currentCount;
+				}
+
+				if (frame === totalFrames) {
+					clearInterval(counter);
+					statValue.innerHTML = statValue.dataset.target;
+				}
+			}, frameDuration);
+		}
+
+		const runAnimations = () => {
+			animateCountUp();
+		}
+		runAnimations();
+	}
+	window.addEventListener('DOMContentLoaded', () => {
+		statValueInit();
+	});
+
+}
+
+const downloadFileOnClick = () => {
+	const buttons = document.querySelectorAll('button[data-download]');
+
+	buttons.forEach(button => {
+		button.addEventListener('click', () => {
+			const fileUrl = button.getAttribute('data-download');
+			const link = document.createElement('a');
+			link.href = fileUrl;
+			link.download = fileUrl.split('/').pop();
+			link.click();
+		});
+	});
+};
